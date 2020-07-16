d1382f54de0a378987c835286ac7d5fd9791f2e8
Sunit Patwardhan <65279280+Sunit130@users.noreply.github.com>
2020-07-16 22:47:26 +0530

    Add files via upload

---
+++ changing-complex-state-practice/src/components/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function updateContact(event) {
+    const { name, value } = event.target;
+    setContact(prev => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prev.lName,
+          email: prev.email
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prev.fName,
+          lName: value,
+          email: prev.email
+        };
+      } else if (name === "email") {
+        return {
+          fName: prev.fName,
+          lName: prev.lName,
+          email: value
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={updateContact}
+          value={contact.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={updateContact}
+          value={contact.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={updateContact}
+          value={contact.email}
+          name="email"
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
